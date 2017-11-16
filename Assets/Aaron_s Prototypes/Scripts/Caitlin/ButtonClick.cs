3552339655b4d2308fd5cb932c94d1902ec2bd4a
Caitlin Rosser <caitlin_rosser@yahoo.co.uk>
2017-11-16 12:42:39 +0000

    Scripts

---
+++ Assets/Aaron_s Prototypes/Scripts/Caitlin/ButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+	public Button shelterButton;
+	RaycastClass ray;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = shelterButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+
+		ray = GameObject.Find("Shelter").GetComponent<RaycastClass>();
+	}
+
+	void TaskOnClick()
+	{
+		ray.isClicked = true;
+		Debug.Log("You have clicked the button!");
+	}
+}
