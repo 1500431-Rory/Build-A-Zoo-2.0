117f714d9943ead887b9450e184fa868cc4ae315
Aaron <1502706@uad.ac.uk>
2017-11-27 19:37:31 +0000

    adding some basic UI functionality

---
+++ Assets/Prototypes/Scripts/Aaron/UI/UIPanelActivate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanelActivate : MonoBehaviour {
+
+    //public Button yourButton;
+    public GameObject Panel;
+
+    //void Start()
+    //{
+    //    Button btn = yourButton.GetComponent<Button>();
+   //     btn.onClick.AddListener(TaskOnClick);
+   // }
+
+   // void TaskOnClick()
+    //{
+        
+   // }
+
+    public void panelManagement()
+    {
+        if(Panel.activeInHierarchy == true)
+        {
+            hidePanel();
+        }
+        else if (Panel.activeInHierarchy == false)
+        {
+            showPanel();
+        }
+    } 
+
+   void showPanel()
+    {
+        Panel.gameObject.SetActive(true);
+    }
+    void hidePanel()
+    {
+        Panel.gameObject.SetActive(false);
+    }
+
+}
