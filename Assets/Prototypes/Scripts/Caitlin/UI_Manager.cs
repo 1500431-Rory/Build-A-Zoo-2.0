d3f8eab46ab1a00065968c861dc5072e1309ee2e
Caitlin Rosser <caitlin_rosser@yahoo.co.uk>
2018-02-01 13:46:52 +0000

    Added sprites to UI buttons and made them clickable

---
+++ Assets/Prototypes/Scripts/Caitlin/UI_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour {
+
+	public Button animal;
+	public Button fence;
+	public Button rock;
+	public Button toy;
+	public Button menu;
+
+	void Start()
+	{
+		// Get Button Component
+		Button animalBtn = animal.GetComponent<Button>();
+		Button fenceBtn = fence.GetComponent<Button>();
+		Button rockBtn = rock.GetComponent<Button>();
+		Button toyBtn = toy.GetComponent<Button>();
+		Button menuBtn = menu.GetComponent<Button>();
+
+		// Add a listenr to button
+		animalBtn.onClick.AddListener(TaskOnClick);
+		fenceBtn.onClick.AddListener(TaskOnClick);
+		rockBtn.onClick.AddListener(TaskOnClick);
+		toyBtn.onClick.AddListener(TaskOnClick);
+		menuBtn.onClick.AddListener(TaskOnClick);
+
+	}
+
+	void TaskOnClick()
+	{
+		
+	}
+}
